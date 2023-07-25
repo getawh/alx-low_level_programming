e8bdfdaa38f3dce12c180a006f6c846fda72d8d7
getawh <61497745+getawh@users.noreply.github.com>
2023-07-25 12:06:12 -0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,9 @@ void puts2(char *str)
 
     while (str[i] != '\0')
     {
-        _putchar(str[i]);
+        putchar(str[i]);
         i += 2;
     }
 
-    _putchar('\n');
+    putchar('\n');
 }
