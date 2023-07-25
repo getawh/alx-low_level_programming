ad8c605debb5411b5f349ce006c9655967f34c45
getawh <61497745+getawh@users.noreply.github.com>
2023-07-25 10:40:48 -0300

    Update 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/* 3-print_numbers.c */
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+    char digit;
+
+    for (digit = '0'; digit <= '9'; digit++)
+        _putchar(digit);
+
+    _putchar('\n');
+}
