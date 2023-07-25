d84893afdeb2b6c08803729af99cf7218e9c361f
getawh <61497745+getawh@users.noreply.github.com>
2023-07-25 11:00:47 -0300

    Update main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/* Function prototypes */
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+
+/* Other declarations and macros if needed */
+
+#endif /* MAIN_H */
